458846db91febc52885b412ba051c26e99974bea
Mark Lanett <mark.lanett@gmail.com>
2011-04-02 14:23:49 -0700

    Initializer function. Usage e.g.: RedisResqueInitializer.initialize_redis_and_resque( ::Rails.root.to_s, ::Rails.env.to_s )

--- lib/redis_resque_initializer.rb
+++ lib/redis_resque_initializer.rb
@@ -1,3 +1,55 @@
+require "yaml"
+
 module RedisResqueInitializer
-  # Your code goes here...
+  
+  def initialize_redis_and_resque( app_root = ".", app_env = "development" )
+    
+    # redis
+    # @see https://github.com/ezmobius/redis-rb
+    # redis options:
+    # host        default "127.0.0.1"
+    # port        default 6379
+    # db          default 0
+    # timeout     default 5
+    # password    no default
+    # logger      no default
+    # thread_safe no default; necessary for threaded environments; what about fibered environments?
+
+    # redis-namespace
+    # @see https://github.com/defunkt/redis-namespace
+    # namespace options:
+    # namespace   no default
+
+    yml    = YAML.load( IO.read( "#{app_root}/config/redis.yml" ) ) [ app_env ]
+    config = Hash[ yml.map { |k,v| [ k.to_sym, v ] } ] # mlanett 2011-02 poor man's symbolize_keys
+
+    redis  = Redis.connect( config )
+    STDERR.puts "[#{Process.pid}] redis_resque_initializer: connecting to #{config.inspect}"
+
+    if namespace = config[:namespace] then
+      $redis = Redis::Namespace.new( namespace, :redis => redis )
+    else
+      $redis = redis
+    end
+
+    # Support Phusion Passenger smart spawning/forking mode.
+    # Reset the connection so it isn"t accidentally shared between multiple processes.
+    begin
+       PhusionPassenger.on_event(:starting_worker_process) do |forked|
+         if forked
+           $redis.client.reconnect
+           STDERR.puts "[#{Process.pid}] redis_resque_initializer: reconnected to #{config.inspect}"
+         end
+       end
+    rescue ArgumentError, NameError => error
+      # NameError: Ruby 1.8
+      # ArgumentError: Ruby 1.9
+      # This statement block intentionally left blank.
+    end
+
+    Resque.redis = $redis
+    
+  end # initialize
+  
+  extend RedisResqueInitializer
 end
--- redis_resque_initializer.gemspec
+++ redis_resque_initializer.gemspec
@@ -6,16 +6,18 @@ Gem::Specification.new do |s|
   s.name        = "redis_resque_initializer"
   s.version     = RedisResqueInitializer::VERSION
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["TODO: Write your name"]
-  s.email       = ["TODO: Write your email address"]
+  s.authors     = ["Mark Lanett"]
+  s.email       = ["mark.lanett@gmail.com"]
   s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
-
-  s.rubyforge_project = "redis_resque_initializer"
-
+  s.summary     = %q{Initializes Redis and Resque}
+  s.description = %Q{RedisResqueInitializer.initialize_redis_and_resque( ::Rails.root.to_s, ::Rails.env.to_s )}
+  
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
+  
+  s.add_dependency "redis"
+  s.add_dependency "redis-namespace"
+  s.add_dependency "resque"
 end
